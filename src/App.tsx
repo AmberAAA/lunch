5138120b4c18ec2a0cfd151cd91037760d7ab9e4
anborong <anbr@novastar.tech>
2021-12-16 11:06:50 +0800

    init launch

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export interface Stats {
+  name: string
 }
 
-export default App;
+export default class App extends React.Component {
+
+  
+  state: Readonly<Stats> = {
+    name: "让我瞅瞅中午吃啥"
+  };
+
+  
+  render(): React.ReactNode {
+    return <h3>Hello World!  {this.state.name}</h3>
+  }
+}
